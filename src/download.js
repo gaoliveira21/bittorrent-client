6aeeec6575c0e7fd7de52eddf1587dfa3e71d09d
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-05-20 15:31:52 -0300

    grouping messages

--- src/download.js
+++ src/download.js
@@ -12,13 +12,32 @@ module.exports = torrent => {
 
 function download(peer) {
   const socket = net.Socket();
-  socket.on('error', console.log);
   socket.connect(peer.port, peer.id, () => {
     // write a message here
 
   });
-  socket.on('data', data => {
-    // handle response here
-
+  onWholeMsg(socket, data => {
+    
   });
+}
+
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+    let handshake = true;
+
+    socket.on('data', recvBuf => {
+      // msgLen calculates the length of a whole msg
+      const msgLen = () => handshake ? 
+      savedBuf.readUInt8(0) + 49 :
+      savedBuf.readInt32BE(0) + 4;
+
+      savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+      while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+
+        callback(savedBuf.slice(0, msgLen()));
+        savedBuf = savedBuf.slice(msgLen());
+        handshake = false;
+      }
+    });
 }
