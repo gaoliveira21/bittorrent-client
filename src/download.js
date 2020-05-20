78ca0fd95f797693e7813da68568ea997243fa78
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-05-20 15:37:05 -0300

    add handshake handler

--- src/download.js
+++ src/download.js
@@ -3,22 +3,30 @@
 const net = require('net');
 const Buffer = require('buffer').Buffer;
 const tracker = require('./tracker');
+const message = require('./utils/messages');
 
 module.exports = torrent => {
   tracker.getPeers(torrent, peers => {
-    peers.forEach(download);
+    peers.forEach(peer => download(peer, torrent));
   })
 };
 
-function download(peer) {
+function download(peer, torrent) {
   const socket = net.Socket();
+  socket.on('error', console.log);
   socket.connect(peer.port, peer.id, () => {
     // write a message here
+    socket.write(message.buildHandshake(torrent))
+  });
+  onWholeMsg(socket, msg => msgHandler(msg, socket));
+}
 
-  });
-  onWholeMsg(socket, data => {
-    
-  });
+function msgHandler(msg, socket) {
+  if(isHandshake(msg)) socket.write(message.buildInterested());
+}
+
+function isHandshake(msg) {
+  return msg.length === msg.readUInt8(0) + 49 && msg.toString('utf8', 1) === 'BitTorrent protocol';
 }
 
 function onWholeMsg(socket, callback) {
